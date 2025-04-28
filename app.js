9cf9d47eafadccff90784c8b888d38b83091af83
rimu-tem <rinrinppcow@gmail.com>
2025-04-28 22:06:22 +0900

    update files

--- app.js
+++ app.js
@@ -5,16 +5,41 @@ $(document).ready(function() {
             center: 'title',
             right: 'month,agendaWeek,agendaDay'
         },
-        events: [
-            {
-                title: '推しのライブイベント',
-                start: '2025-05-10T19:00:00',
-                description: '詳細な説明',
-                color: '#ff5733' // 推しカラー（色指定）
-            }
-        ],
         dayClick: function(date, jsEvent, view) {
-            alert('選択した日付: ' + date.format());
+            // モーダルを表示
+            $('#eventModal').show();
+            $('#eventForm').data('date', date.format());  // クリックした日付をフォームに記録
         }
+    });
+
+    // モーダルを閉じる
+    $('.close').on('click', function() {
+        $('#eventModal').hide();
+    });
+
+    // 予定追加フォーム送信時
+    $('#eventForm').on('submit', function(event) {
+        event.preventDefault();  // フォーム送信を防ぐ
+
+        // 入力データ取得
+        var eventName = $('#eventName').val();
+        var eventDetails = $('#eventDetails').val();
+        var eventTime = $('#eventTime').val();
+        var eventColor = $('#eventColor').val();
+        var eventDate = $('#eventForm').data('date') + 'T' + eventTime + ':00';
+
+        // 予定をFullCalendarに追加
+        $('#calendar').fullCalendar('renderEvent', {
+            title: eventName,
+            start: eventDate,
+            description: eventDetails,
+            color: eventColor
+        });
+
+        // モーダルを閉じる
+        $('#eventModal').hide();
+
+        // フォームをリセット
+        $('#eventForm')[0].reset();
     });
 });
